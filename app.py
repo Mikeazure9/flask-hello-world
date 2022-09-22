8f4314490dca7a4f65c0f10395837696ff2be8c4
Mikeazure9 <113344823+Mikeazure9@users.noreply.github.com>
2022-09-21 19:56:23 -0700

    update home page

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+@app.route('/home')
+def read():
+    if not name and password:
+        return redirect('/')
